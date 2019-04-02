fbff19ea2c258f9af512686068511ab78de98173
elizabeth <elizabethsanchez1@live.com>
2019-04-01 19:20:55 -0500

    refactoring

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,8 +1,6 @@
-const letters = document.querySelectorAll('.letters');
 const submitBtn = document.querySelector('.submit-btn');
 const deleteBtn = document.querySelector('.delete-btn');
 const restartBtn = document.querySelector('.restartbtn');
-let guessList = document.querySelector('.list-of-guesses'); 
 let word = localStorage.getItem('word');
 let feedback = document.querySelector('.feed-back');
 const letterGuesses = [];
@@ -10,103 +8,59 @@ const letterGuesses = [];
 
 
 localStorage.getItem('word');
-console.log("got word", localStorage.getItem('word'));
+let guessList = document.querySelector('.list-of-guesses');
 
-// function displayGuesses() {
-//   // letterGuesses;
-//   // guessesContainer.innerHTM = ""; 
-//   console.log("displayGuesses is working");
-// };
-
-
-
-console.log("letters:", letters);
+const letters = document.querySelectorAll('.letters');
 for (i = 0; i < letters.length; i++) {
-  letters[i].addEventListener('click', function(event) {
-    letterGuesses.push(event.target.innerHTML);
-    // console.log("event", event);
-    // console.log("event-target", event.target.innerHTML);
-    console.log("guesses", letterGuesses);
-    //displayGuesses();
-    let singleGuess = document.createElement('li');   
-    singleGuess.innerHTML = event.target.innerHTML;
-    guessList.insertAdjacentElement('beforeend', singleGuess);
-   
-    //console.log("---------singleGuess------------", singleGuess);
-    console.log("----word-----", word);
-    
-    
-    let correctGuess = true;
-    
-    for (i = 0; i < letterGuesses.length; i++) {
-            
-      if (letterGuesses[i].toLowerCase() !== word[i]) {
-        correctGuess = false;
-        feedback.innerHTML = 'OOPS Guess Again!';
-        console.log('not all guesses are correct');
-        
-      } else {
-        feedback.innerHTML = 'Good Guess!';
-      }
-      
-    };
-    
-  
-   
-    
-    
- 
-  // console.log('letter has been clicked', event);
-   console.log('event target', event.target.innerHTML); 
- });
+	letters[i].addEventListener('click', function (event) {
+		letterGuesses.push(event.target.innerHTML);
+		let singleGuess = document.createElement('li');
+		singleGuess.innerHTML = event.target.innerHTML;
+		guessList.insertAdjacentElement('beforeend', singleGuess);
+
+		let correctGuess = true;
+
+		for (i = 0; i < letterGuesses.length; i++) {
+
+			if (letterGuesses[i].toLowerCase() !== word[i]) {
+				correctGuess = false;
+				feedback.innerHTML = 'OOPS Guess Again!';
+				console.log('not all guesses are correct');
+
+			} else {
+				feedback.innerHTML = 'Good Guess!';
+			}
+
+		};
+
+
+		console.log('event target', event.target.innerHTML);
+	});
 };
 
+submitBtn.addEventListener('click', function () {
+	let guesses = letterGuesses.join('').toLowerCase();
 
-submitBtn.addEventListener('click', function() {
-  let guesses = letterGuesses.join('').toLowerCase();
-  
-  
-  console.log("I'm the word", word);
-  console.log('-------single string:-----', guesses);
-  if (guesses === word) {
-    feedback.innerHTML = "WINNER!";
-   
-  } else {
-    feedback.innerHTML = "Try Again!";
-  }
-    
-  
 
+	console.log("I'm the word", word);
+	console.log('-------single string:-----', guesses);
+	if (guesses === word) {
+		feedback.innerHTML = "WINNER!";
+
+	} else {
+		feedback.innerHTML = "Try Again!";
+	}
 
 });
 
+deleteBtn.addEventListener('click', function () {
+	let guessList = document.querySelector('.list-of-guesses');
+	letterGuesses.pop();
+	guessList.removeChild(guessList.lastChild);
 
-deleteBtn.addEventListener('click', function() {
-  console.warn('letter guesses before pop: ', letterGuesses);
-  letterGuesses.pop();
-  //console.log("---guesslist last child---", guessList.lastChild);
-  guessList.removeChild(guessList.lastChild);
-  console.log("deleted letter-----", letterGuesses);
-//   console.log('delete button was clicked');
-//   console.log("-----Last child-------", guessList.lastElementChild);
 });
 
-
-restartBtn.addEventListener('click', function() {
-  console.log('restart was clicked');
-  window.location.href ="index.html";
+restartBtn.addEventListener('click', function () {
+	console.log('restart was clicked');
+	window.location.href = "index.html";
 });
-
-
-
-
-/* 
-
-On submit check whether the user has spelled the word correctly.
-
-Use an alert box to tell the user whether they are right or wrong.
-
-
-
-
-*/
--- scripts/index.js
+++ scripts/index.js
@@ -16,26 +16,18 @@
 */
 
 
-
 const sighWords = ["see", "like", "we", "what", "me", "find", "now", "will", "this", "make", "she", "play", "down", "said", "help", "look", "have", "is", "he", "test"];
 console.log("normal one: ", sighWords);
 const rightArrow = document.querySelector('.fa-caret-right');
 const leftArrow = document.querySelector('.fa-caret-left');
-
 const startbtn = document.querySelector('.startbtn');
-const hiddenItems = document.querySelectorAll('.hidden');
+
 const playActivity = document.querySelector('.play-activity');
 let currentIndex = 0;
 let shuffledArray = [];
 
 
 playActivity.addEventListener('click', function () {
-	console.log('playactivity link was clicked', playActivity);
-	// localStorage.setItem('selectedWord', shuffledArray[currentIndex]);
-
-	console.log('array valiue: ', shuffledArray[currentIndex])
-
-	// localStorage['word'] = shuffledArray[currentIndex]; 
 	localStorage.word = shuffledArray[currentIndex];
 
 });
@@ -54,12 +46,12 @@ function getShuffledArray(arr) {
 function updateCenterText(word) {
 	const wordContainer = document.querySelector('.word');
 	wordContainer.innerHTML = word;
-		
+
 }
 
 startbtn.addEventListener('click', function () {
+	const hiddenItems = document.querySelectorAll('.hidden');
 	shuffledArray = getShuffledArray(sighWords);
-	console.log("shuffled:", shuffledArray);
 	updateCenterText(shuffledArray[currentIndex]);
 	console.log(hiddenItems);
 	hiddenItems.forEach(function (items) {
@@ -70,23 +62,19 @@ startbtn.addEventListener('click', function () {
 
 
 rightArrow.addEventListener('click', function () {
-	console.log("current index:", currentIndex);
 
-	console.log("newWord; ", shuffledArray[currentIndex]);
 	if (currentIndex > shuffledArray.length - 2) {
 		currentIndex = -1;
 	}
-
 	currentIndex = currentIndex + 1;
 	let nextWord = shuffledArray[currentIndex];
-	
 	updateCenterText(nextWord);
 
-	console.log("second current index:", currentIndex);
 });
 
 
 leftArrow.addEventListener('click', function () {
+
 	if (currentIndex === 0) {
 		let lastWord = shuffledArray[shuffledArray.length - 1];
 		updateCenterText(lastWord);
@@ -94,17 +82,7 @@ leftArrow.addEventListener('click', function () {
 		return;
 	}
 	currentIndex = currentIndex - 1;
-	console.log("previous word index:", currentIndex);
 	let previousWord = shuffledArray[currentIndex];
-	
 	updateCenterText(previousWord);
-	
+
 });
-
-
-// function getLastIndex() {
-//   return shuffledArray.length - 1;
-// }
-
-// console.log(shuffledArray[shuffledArray.length-1]);
-// console.log(shuffledArray[getLastIndex()]);
